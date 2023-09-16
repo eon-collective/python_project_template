aec4d0825131ffc20c8c88b13fd2c79d31366c73
GeorgeEngEon <george.njihia@eoncollective.com>
2023-09-16 18:32:13 +0300

    Team Assessment

--- src/modules/process_team_summary.py
+++ src/modules/process_team_summary.py
@@ -1,14 +1,34 @@
+"""This script contain modular function to process employees details."""
+
 import json
 import os
 
+
 def read_data(path):
+    """Read JSON data from a file located at the specified path.
+
+    Args:
+        path (str): The directory path where the JSON file is located.
+
+    Returns:
+        dict: A Python dictionary containing the parsed JSON data from the file.
+    """
     filename = "teamke.json"
     full_path = os.path.join(path, filename)
-    with open(full_path, 'r') as file:
+    with open(full_path, "r") as file:
         data = json.load(file)
     return data
 
+
 def introduce_team(data):
+    """Display team size and role distribution.
+
+    Args:
+        data (list of dict): List of dictionaries with "Name" and "Role" keys.
+
+    Returns:
+        dict: Role-based team members.
+    """
     team_size = len(data)
 
     print()
@@ -16,17 +36,26 @@ def introduce_team(data):
     print()
     role_groups = {}
     for person in data:
-        role = person['Role']
+        role = person["Role"]
         if role in role_groups:
-            role_groups[role].append(person['Name'])
+            role_groups[role].append(person["Name"])
         else:
-            role_groups[role] = [person['Name']]
+            role_groups[role] = [person["Name"]]
 
-    # Print the grouped data
     for role, people in role_groups.items():
-        print(f'Role: {role}, People: {len(people)}')
+        print(f"Role: {role}, People: {len(people)}")
+
+    return role_groups
+
 
 def give_details(data):
+    """Print details of team members and their roles.
+
+    Args:
+        data (list of dict): List of dictionaries with "Name" and "Role" keys.
+    """
     print()
     for record in data:
-        print(f'We have {record["Name"]} who works as {record["Role"]}.')
+        print(f'{record["Name"]} works as {record["Role"]}.')
+
+    return
