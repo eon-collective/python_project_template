65b8d85b00624d7e596edbd7a24996a66afb15a7
jameseon <87770464+jameseon@users.noreply.github.com>
2023-10-19 18:10:04 -0400

    Update main.py
    Added program name, version and epilog for arg parser

--- src/main.py
+++ src/main.py
@@ -6,8 +6,13 @@ from modules.process_team_summary import introduce_team  # D
 from modules.process_team_summary import give_details, read_data
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process a summary for team kenya.")
+    parser = argparse.ArgumentParser(
+        prog='hello_world'
+        description='''Process a summary for team kenya.'''
+        epilog='ADEPT Utilities')
 
+    # Add application arguments
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
     parser.add_argument("--team_ke_json", type=str, help="json path")
     args = parser.parse_args()
     team_ke_json = args.team_ke_json
